571e0a29efe38aa8925935ab51f28b1e13c344b5
Babul Akter <babulakterfsd@gmail.com>
2024-02-29 16:16:06 +0600

    footer added

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,35 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+
 const Footer = () => {
   return (
-    <div>
-      <h1>This is the footer</h1>
+    <div className="bg-black py-20">
+      <div className="main-container text-white flex justify-between items-center">
+        {/* logo */}
+        <div>
+          <Link className="flex gap-x-2 items-center w-24" to="/">
+            <img src={logo} alt="GizmoBuy" className="w-8 h-8 object-cover" />
+            <span className="font-bold text-xl">GizmoBuy</span>
+          </Link>
+          <div>
+            <p className="text-offgray text-sm mt-6">Customer Support:</p>
+            <p className="">+88-01740-020464</p>
+          </div>
+          <div className="my-6 text-sm text-offgray">
+            Jinjirtala, Dhunat Pouroshava <br />
+            Dhunat 5850, Bogura, Bangladesh
+          </div>
+          <div>babulakterfsd@gmail.com</div>
+        </div>
+        {/* top categories */}
+        <div>top categoris</div>
+        {/* quick links */}
+        <div>quick links</div>
+        {/* downlaod */}
+        <div>downlaod app</div>
+        {/* popular tags */}
+        <div>popular tags</div>
+      </div>
     </div>
   );
 };
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -112,9 +112,9 @@ const Navbar = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 4 4 4-4"
                 />
               </svg>
