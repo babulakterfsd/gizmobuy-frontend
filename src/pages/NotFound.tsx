f422879518813a2561312fc11e9774029878254e
Babul Akter <babulakterfsd@gmail.com>
2024-03-03 13:31:21 +0600

    not found page redesigned

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,19 +1,30 @@
+import { FaArrowRightLong } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import notFoundImg from '../assets/images/404.png';
 
 const NotFound = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <h1 className="text-red-700 text-3xl text-center">Page Not Found !</h1>
-      <p className="text-center text-sm lg:w-96 mt-2">
-        This page is not found. Please go back to home page.
+      <img
+        src={notFoundImg}
+        alt="Not Found"
+        className="object-cover w-96 h-96"
+      />
+      <h3 className="text-orange text-2xl font-semibold">
+        {' '}
+        404 ! Page Not Found !!
+      </h3>
+      <p className="text-sm text-center mt-4 w-full sm:w-[475px]">
+        Something went wrong. It’s look that your requested could not be found.
+        It’s look like the link is broken or the page is removed.
       </p>
-      <Link
-        to="/"
-        className="py-2 px-3 bg-red-300 rounded-md text-white mt-4"
-        aria-current="page"
-      >
-        Home
-      </Link>
+      <div className="mt-4">
+        <Link to="/">
+          <button className="bg-deep-yellow py-2.5 px-6 rounded text-black font-semibold flex items-center gap-x-2">
+            Back to Home <FaArrowRightLong />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
