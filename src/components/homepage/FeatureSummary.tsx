aae46da9b441eeffa8fbecae3aad79c8e7d162e2
Babul Akter <babulakterfsd@gmail.com>
2024-03-23 16:00:43 +0600

    navbar responsive done

--- src/components/homepage/FeatureSummary.tsx
+++ src/components/homepage/FeatureSummary.tsx
@@ -5,7 +5,7 @@ import trophy from '../../assets/images/Trophy.svg';
 
 const FeatureSummary = () => {
   return (
-    <div className="mt-6 border border-gray-100 rounded-md w-full flex justify-around flex-col lg:flex-row gap-y-8 py-6 px-10 lg:p-6">
+    <div className="mt-14 lg:mt-20 border border-gray-100 rounded-md w-full flex justify-around flex-col lg:flex-row gap-y-8 py-6 px-10 lg:p-6">
       {/* feature 1 */}
       <div className="flex space-x-4">
         <img
