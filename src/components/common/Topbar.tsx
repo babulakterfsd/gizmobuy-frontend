38d4dffb6265e297de321edb5ae553d2d2905e40
Babul Akter <babulakterfsd@gmail.com>
2024-03-23 14:01:12 +0600

    topbar responsive done

--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -13,27 +13,29 @@ const Topbar = () => {
   if (!showTopbar) return null;
 
   return (
-    <div className="bg-custom-black">
-      <div className="main-container flex justify-between items-center text-white h-16">
-        <div className="flex gap-x-2">
+    <div className="bg-custom-black text-sm lg:text-base">
+      <div className="main-container flex justify-start lg:justify-between items-center text-white py-4 md:py-2 lg:py-0 lg:h-16 gap-x-7 md:space-x-28 md:gap-x-0">
+        <div className="flex gap-x-1 lg:gap-x-2">
           <p className="bg-yellow text-custom-black px-2 py-1 -rotate-6 rounded font-semibold">
             Black
           </p>
           <span>Friday</span>
         </div>
-        <div className="flex gap-x-2 items-center">
-          <span>Up to</span>{' '}
-          <span className="text-4xl text-deep-yellow font-semibold">50%</span>{' '}
-          <span>OFF</span>
+        <div className="flex gap-x-1 lg:gap-x-2 items-center">
+          <span className="hidden md:inline-block">Up to</span>{' '}
+          <span className="text-xl md:text-2xl lg:text-4xl text-deep-yellow font-semibold">
+            50%
+          </span>{' '}
+          <span className="text-sm md:text-base">OFF</span>
         </div>
         <div>
           <Link to="/shop">
-            <button className="bg-deep-yellow py-2.5 px-6 rounded text-custom-black font-semibold flex items-center gap-x-2">
+            <button className="bg-deep-yellow py-2 lg:py-2.5 px-2 lg:px-6 rounded text-custom-black font-semibold flex items-center gap-x-1 md:gap-x-2">
               Shop Now <FaArrowRightLong />
             </button>
           </Link>
         </div>
-        <div className="absolute right-3">
+        <div className="absolute right-1 md:right-3">
           <button className="bg-gray p-1" onClick={handleShowTopbar}>
             <RxCross2 />
           </button>
