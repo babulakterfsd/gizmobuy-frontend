bbda57971f95f3075d92ee09b21ea310edfb8f52
Babul Akter <babulakterfsd@gmail.com>
2024-04-01 12:22:21 +0600

    faq section completed

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -4,8 +4,15 @@ const Faq = () => {
   return (
     <div>
       <ScrollToTop />
-      <div className="main-container mt-14 lg:mt-20 pb-6 lg:pb-10">
-        <p>FAQ page</p>
+      <div className="main-container mt-6 lg:mt-10 pb-6 lg:pb-10 grid grid-cols-1 md:grid-cols-12">
+        <div className="col-span-12 md:col-span-7">
+          <h3 className="text-custom-black text-3xl font-semibold mb-10">
+            Frequently Asked Questions
+          </h3>
+        </div>
+        <div className="col-span-12 md:col-span-5">
+          <p>test 2</p>
+        </div>
       </div>
     </div>
   );
