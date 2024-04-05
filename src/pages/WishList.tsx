5d93c17963a84e4859486a974351170327b781e6
Babul Akter <babulakterfsd@gmail.com>
2024-04-06 01:01:24 +0600

    wishlist tablet  done

--- src/pages/WishList.tsx
+++ src/pages/WishList.tsx
@@ -34,26 +34,32 @@ const WishList = () => {
   return (
     <div>
       <ScrollToTop />
-      <div className="main-container mt-8 lg:mt-24 lg:px-32">
+      <div className="main-container mt-8 md:mt-24 lg:px-32">
         {wishedProducts.length > 0 ? (
           <div className="border border-gray-300 rounded">
-            <h4 className="text-custom-black font-semibold py-3 px-6 border border-gray-300">
+            <h4 className="text-custom-black font-semibold py-3 px-4 lg:px-6 border border-gray-300">
               Wishlist
             </h4>
-            <table className="w-full text-left rtl:text-right text-gray-500 px-6">
+            <table className="w-full text-left rtl:text-right text-pure-gray px-4 lg:px-6">
               <thead className="text-pure-gray text-xs uppercase bg-gray-100 ">
                 <tr>
-                  <th scope="col" className="px-6 py-3"></th>
-                  <th scope="col" className="px-6 py-3">
+                  <th
+                    scope="col"
+                    className="px-4 lg:px-6 py-3 hidden lg:inline-block"
+                  ></th>
+                  <th
+                    scope="col"
+                    className="pl-2 lg:px-6 py-3 text-center lg:text-left"
+                  >
                     Product
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-4 lg:px-6 py-3 text-center">
                     Price
                   </th>
-                  <th scope="col" className="px-6 py-3">
+                  <th scope="col" className="px-4 lg:px-6 py-3 text-center">
                     Availability
                   </th>
-                  <th scope="col" className="px-6 py-3 text-center">
+                  <th scope="col" className="px-4 lg:px-6 py-3 text-center">
                     Actions
                   </th>
                 </tr>
@@ -64,30 +70,30 @@ const WishList = () => {
                     className="bg-white border-b  hover:bg-orange-50 transition-all duration-300 ease-in-out"
                     key={product._id}
                   >
-                    <td className="px-6 py-4 font-medium text-gray-700 whitespace-nowrap">
+                    <td className="lg:px-6 py-4 font-medium text-gray-700 whitespace-nowrap hidden lg:inline-block">
                       <img
                         src={product.displayImage}
                         alt={product.title}
                         className="w-16 h-16 object-contain"
                       />
                     </td>
-                    <td className="px-6 py-4 font-semibold text-pure-gray text-sm whitespace-nowrap">
+                    <td className="px-3 lg:px-6 py-4 font-semibold text-pure-gray text-sm whitespace-nowrap">
                       {product?.title}
                     </td>
                     <td
                       scope="row"
-                      className="px-6 py-4 text-pure-gray text-sm font-semibold"
+                      className="lg:px-6 py-4 text-pure-gray text-sm font-semibold text-center"
                     >
                       {`$${product.price}`}
                     </td>
-                    <td className="px-6 py-4">
+                    <td className="lg:px-6 py-4 text-center">
                       {product.stock > 0 ? (
                         <span className="text-green-400">in-stock</span>
                       ) : (
                         <span className="text-red-400">out-of-stock</span>
                       )}
                     </td>
-                    <td className="px-6 py-4 flex space-x-4 justify-center items-center mt-5">
+                    <td className="lg:px-6 py-4 flex space-x-4 justify-center items-center mt-5">
                       <Link to={`/product/${product?._id}`}>
                         <button
                           className="border border-gray-300 p-2 rounded-full"
@@ -100,7 +106,9 @@ const WishList = () => {
                         className="bg-orange text-white py-1.5 px-3 rounded flex justify-center items-center space-x-1"
                         title="add to cart"
                       >
-                        <span>Add to cart</span>
+                        <span className="text-sm lg:text-base">
+                          Add to cart
+                        </span>
                         <CiShoppingCart className="font-semibold text-lg" />
                       </button>
                       <button
