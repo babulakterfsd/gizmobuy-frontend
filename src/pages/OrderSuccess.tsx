2bb194e87e2703a9527bcf483e46b7ed1b813ed1
Babul Akter <babulakterfsd@gmail.com>
2024-04-18 16:09:37 +0600

    checkout page designed

--- src/pages/OrderSuccess.tsx
+++ src/pages/OrderSuccess.tsx
@@ -1,9 +1,12 @@
+import ScrollToTop from '@/components/ui/ToTop';
 import { useCurrentUser } from '@/redux/features/authSlice';
 import { usePaymentCalculation } from '@/redux/features/paymentSlice';
 import { useShoppingCartProducts } from '@/redux/features/shoppingCartSlice';
 import { useAppSelector } from '@/redux/hook';
 import { TCurrentUser } from '@/types/commonTypes';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import checkcircle from '../assets/images/CheckCircle.png';
+import stack from '../assets/images/Stack.png';
 import NotFound from './NotFound';
 
 const OrderSuccess = () => {
@@ -22,7 +25,29 @@ const OrderSuccess = () => {
 
   return (
     <div>
-      <h2>Your order has been placed successfully</h2>
+      <ScrollToTop />
+      <div className="main-container">
+        <div className="flex justify-center h-[75vh] items-center flex-col">
+          <img
+            src={checkcircle}
+            alt="check-circle"
+            className="w-12 md:w-20 h-12 md:h-20"
+          />
+          <h3 className="text-custom-black text-xl md:text-2xl font-semibold mt-6 text-center">
+            Your order has been successfully placed!
+          </h3>
+          <p className="text-offgray text-sm md:w-7/12 lg:w-5/12 text-center mt-2">
+            Pellentesque sed lectus nec tortor tristique accumsan quis dictum
+            risus. Donec volutpat mollis nulla non facilisis.
+          </p>
+          <Link to={`/dashboard/${currentUser?.role}/manage-orders`}>
+            <button className="py-2 lg:py-2.5 px-3 lg:px-6 rounded text-orange-400 font-semibold flex items-center justify-center gap-x-2 w-full mt-6 text-center text-nowrap border-2 border-orange-400 hover:bg-orange-400 hover:text-white transition-all duration-300">
+              <img src={stack} alt="stack" className="w-5 h-5" />
+              View Orders
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
