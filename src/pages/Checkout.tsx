eb3481994d33bbf81504ed8ce785f887884c0efc
Babul Akter <babulakterfsd@gmail.com>
2024-05-14 11:32:20 +0600

    orders api slice created

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -150,7 +150,7 @@ const Checkout = () => {
                 <input
                   type="text"
                   id="name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500  focus:outline-none"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500  focus:outline-none cursor-not-allowed"
                   placeholder={currentUser?.name}
                   disabled={true}
                 />
@@ -165,7 +165,7 @@ const Checkout = () => {
                 <input
                   type="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500  focus:outline-none"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500  focus:outline-none cursor-not-allowed"
                   placeholder={currentUser?.email}
                   disabled={true}
                 />
