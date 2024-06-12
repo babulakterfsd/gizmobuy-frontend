febad57e539625d70c549bda192259676c83ff35
Babul Akter <babulakterfsd@gmail.com>
2024-06-12 12:12:19 +0600

    delete product added for vendor

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
     'no-mixed-spaces-and-tabs': ['warn', 'smart-tabs'],
